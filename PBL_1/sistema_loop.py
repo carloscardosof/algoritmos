1c4d0465e4a718fc02a735c9cff34a787d1bcf36
Carlos Cardoso <carlos.filho@undb.edu.br>
2025-09-02 17:36:18 -0300

    Update atividades and sistema_loop.py

---
+++ PBL_1/sistema_loop.py
@@ -0,0 +1,12 @@
+print("Seja bem vindo à Papa Léguas Express!");
+
+entrada = 'ativo'
+
+while entrada != 'terminar':
+
+    entrada = input('Qual o seu pedido?\n')
+
+
+
+
+print('Obrigado por utilizar nosso sistema!')
